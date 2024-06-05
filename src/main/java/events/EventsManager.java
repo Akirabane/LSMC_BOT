6ea2ca00f5f3c13386e0da62e532a8341d5fe4a8
Akirabane <joshuabenon988@yahoo.fr>
2024-06-05 13:47:15 +0200

    Ajout passage de douane
    Verification des utilisateur via une reaction dans le channel Douane

--- src/main/java/events/EventsManager.java
+++ src/main/java/events/EventsManager.java
@@ -1,5 +1,6 @@
 package events;
 
+import listeners.VerificationListener;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -8,6 +9,7 @@ public class EventsManager extends ListenerAdapter {
         public EventsManager() {}
         public void registerAllListeners(JDABuilder builder) {
             builder.addEventListeners(new GuildMemberJoinListener());
+            builder.addEventListeners(new VerificationListener());
         }
 
 }
